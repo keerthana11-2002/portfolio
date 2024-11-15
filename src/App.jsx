cbd61fb5413a8054e5a7354965f3705de888e195
keerthana11-2002 <jkkeerthana10@gmail.com>
2024-11-15 15:34:08 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return (
@@ -9,9 +10,9 @@ const App = () => {
         <Navbar />
         <Hero/>
       </section>
-      <section id="Skills">Parallax</section>
+      <section id="Skills" ><Parallax type="skills"/></section>
       <section>Skills</section>
-      <section id="Projects">parallax</section>
+      <section id="Projects" ><Parallax type="projects"/></section>
       <section>project1</section>
       <section>project2</section>
       <section>project3</section>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,29 +1,83 @@
 import React from "react";
 import "./Hero.scss";
+import { motion } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const slideVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType:"mirror",
+      duration: 20,
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="text">
-        <div class="hero-content">
-          <h1>
-            Hi! I'm <span class="highlight">Keerthana</span>
-          </h1>
-          <h2 class="role">Frontend Developer</h2>
-          <p class="description">
-            I’m passionate about crafting responsive, user-friendly interfaces
-            with modern web technologies. Turning ideas into visually appealing
-            and functional designs is my forte, ensuring seamless user
-            experiences.
-          </p>
-        </div>
-      </div>
+      <motion.div
+        className="hero-content"
+        variants={variants}
+        initial="initial"
+        animate="animate"
+      >
+        <motion.h1 variants={variants}>
+          Hi! I'm <span class="highlight">Keerthana</span>
+        </motion.h1>
+        <motion.h2 className="role" variants={variants}>
+          Frontend Developer
+        </motion.h2>
+        <motion.p className="description" variants={variants}>
+          I'm passionate about crafting responsive, user-friendly interfaces
+          with modern web technologies. Turning ideas into visually appealing
+          and functional designs is my forte, ensuring seamless user
+          experiences.
+        </motion.p>
+        <motion.div className="links-button" variants={variants}>
+          <button className="download">Download CV</button>
+          <button className="connect">Let's Connect</button>
+        </motion.div>
+        <motion.img
+          className="scroll"
+          src="scroll.png"
+          alt=""
+          variants={variants}
+          animate="scrollButton"
+        />
+      </motion.div>
+
+      <motion.div className="slidingcontainer" variants={slideVariants} initial="initial" animate="animate">
+        Design. Code. Create. Innovate. Collaborate. Deliver.
+      </motion.div>
       <div className="imagecontainer">
-        <img src="banner.png" />
-      </div>
-      <div className="links-button">
-<button className="download">Download CV</button>
-<button className="connect">Let's Connect</button>
+        <img src="banner.png" alt="" />
       </div>
     </div>
   );
